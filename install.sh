3ffc7d4aa7796c89ae5ac5282473e9e9239ce809
Salih <salihozen03@hotmail.com>
2019-04-13 13:01:56 +0300

    Removed appjar from repository and merged Arch and Debian installation

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-sudo apt-get install python3 nodejs npm python3-tk
+if apt-get -v > /dev/null; then sudo apt-get -y install python3 python-pip python3-tk nodejs npm tk build-essential libudev-dev; fi
+if pacman -V > /dev/null; then sudo pacman -Sy --noconfirm --needed python3 python-pip nodejs npm tk base-devel; fi
 sudo mkdir /opt/MonsterKeyboard
 sudo cp -r ./* /opt/MonsterKeyboard
 sudo cp ./.node-version /opt/MonsterKeyboard
 cd /opt/MonsterKeyboard
+sudo pip3 install appjar
 sudo npm install
 sudo mv /opt/MonsterKeyboard/MonsterKeyboard.desktop /usr/share/applications
 sudo mv /opt/MonsterKeyboard/monster-kb.service /lib/systemd/system
