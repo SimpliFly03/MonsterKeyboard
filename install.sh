ca0d2e051139b70d3f644f797e4d21fbc2ca5c2f
SimpliFly03 <48456521+SimpliFly03@users.noreply.github.com>
2020-03-15 14:59:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-if apt-get -v > /dev/null; then sudo apt-get -y install python3 python-pip python3-tk nodejs npm tk build-essential libudev-dev; fi
+if apt-get -v > /dev/null; then sudo apt-get -y install python3 python-pip python3-pip python3-tk nodejs npm tk build-essential libudev-dev; fi
 if pacman -V > /dev/null; then sudo pacman -Sy --noconfirm --needed python3 python-pip nodejs npm tk base-devel; fi
 sudo mkdir /opt/MonsterKeyboard
 npm install
